a8b8c144dec00a6574075f61ed1dca21661c73d1
jobort <jorbort92@gmail.com>
2024-02-01 23:39:30 +0100

    auto commit

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,14 +1,32 @@
 #include "program.hpp"
 
-void ft_replace(std::string &line, char *old, char *replace)
+void ft_replace(std::string &line, std::string s1, std::string s2)
 {
+	int last = 0;
+	size_t find_value;
+	std::string aux;
 
+	while((find_value = line.find(s1,last)) != std::string::npos)
+	{
+		aux = line.substr(find_value + s1.size());
+		line.erase(find_value);	
+		line += s2 + aux;	
+		last = find_value +s2.size();
+	}
 }
 
 int main(int argc, char **argv)
 {
 	if (argc != 4)
+	{
+		std::cout << "wrong amount of arguments" << std::endl;
 		return (1);
+	}
+	if (!argv[2][0] || !argv[3][0])
+	{
+		std::cout << "arguments cant be empty" << std::endl;
+		return (1);
+	}
 	std::ifstream infile;
 	std::ofstream outfile;
 	std::string new_filename = argv[1];
@@ -22,7 +40,7 @@ int main(int argc, char **argv)
 		std::cout << "unable to open file" << std::endl;
 		return (1);
 	}
-	outfile.open(new_filename, std::ios::out);
+	outfile.open(new_filename.c_str(), std::ios::out);
 	if (outfile.good() == false)
 	{
 		infile.close();
